a5f710b34be6a143301cc3a0642b2db826477dba
iamodog <joran.mr@icloud.com>
2020-04-10 09:14:13 +0200

    apply function in TeamType

--- backend/tests/tests_unitaires/tests_TeamType.py
+++ backend/tests/tests_unitaires/tests_TeamType.py
@@ -8,6 +8,8 @@ class TeamTypeTests(TestCase):
         perm_1 = Permission.objects.get(id=1)
         perm_2 = Permission.objects.get(id=2)
         admin_type = TeamType.objects.create(name="Administrators")
+        admin_team = Team.objects.create(name="Administrators")
+        admin_team.set_team_type(admin_type)
         admin_type.perms.add(perm_1, perm_2)
 
     def test_get_team_type(self):
@@ -22,9 +24,15 @@ class TeamTypeTests(TestCase):
 
     def test_apply_(self):
         admin_type = TeamType.objects.get(name="Administrators")
-        admin_team = Team.objects.create(name="Administrators",
-                                        team_type=admin_type)
-        admin_type.apply()
-
+        admin_type._apply_()
+        admin_team = Team.objects.get(name="Administrators")
         self.assertEqual(admin_team.permissions.get(id=1),Permission.objects.get(id=1))
         self.assertEqual(admin_team.permissions.get(id=2), Permission.objects.get(id=2))
+
+    def test_apply_new_teamtype(self):
+        perm_3 = Permission.objects.get(id=3)
+        MaintenanceManager_type = TeamType.objects.create(name="Maintenance Manager")
+        MaintenanceManager_type.perms.add(perm_3)
+        admin_team = Team.objects.get(name="Administrators")
+        admin_team.set_team_type(MaintenanceManager_type)
+        self.assertEqual(admin_team.permissions.get(id=3), perm_3)
--- backend/usersmanagement/models.py
+++ backend/usersmanagement/models.py
@@ -37,12 +37,11 @@ class TeamType(models.Model):
     def __str__(self):
         return self.name
 
-    def apply(self):
-        teams_with_this_teamtype = Team.objects.filter(team_type=self)
+    def _apply_(self):
+        teams_with_this_teamtype = self.team_set.all()
         for team in teams_with_this_teamtype:
             team.permissions.set(list(self.perms.all()))
-            # for perm in self.perms.all():’
-            #     team.permissions.add(perm)
+
 
 
 
@@ -54,3 +53,8 @@ class Team(Group):
         help_text='Group of users, extends the auth.models.Group model',
         related_name="team_set",
         related_query_name="team")
+
+    def set_team_type(self,new_team_type):
+        self.team_type = new_team_type
+        self.save()
+        new_team_type._apply_()
