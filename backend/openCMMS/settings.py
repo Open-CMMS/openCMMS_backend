b9649c752e318f19168a3212eb1cda8c2d6c4ba1
iamodog <joran.mr@icloud.com>
2020-04-10 14:19:34 +0200

    Remise dans la bonne bd dans settings.py

--- backend/openCMMS/settings.py
+++ backend/openCMMS/settings.py
@@ -77,21 +77,13 @@ WSGI_APPLICATION = 'openCMMS.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'django',
-    #     'USER': 'django',
-    #     'PASSWORD': 'django',
-    #     'HOST': '192.168.101.11',
-    #     'PORT': '',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'sqlite3.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': '192.168.101.11',
+        'PORT': '',
     }
 }
 
