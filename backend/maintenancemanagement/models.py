06d54f32d9f866d913414a3fa3483d958d9e375e
cassou <cassou@pop-os.localdomain>
2020-04-27 15:22:21 +0200

    ajout model equipment #4gr9ap

--- backend/maintenancemanagement/models.py
+++ backend/maintenancemanagement/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models import FileField
 
-# Create your models here.
+class Equipment(models.Model):
+    """
+        Define an equipment.
+    """
+    name = models.CharField(max_length=100)
+    equipment_type = models.ForeignKey(EquipmentType,
+        verbose_name = "Equipment Type",
+        on_delete = models.DELETE_CASCADE,
+        null = False,
+        related_name = "equipment_set",
+        related_query_name="equipment")
+    #gestion d'un fichier, voir pour en gérer plusieurs
+    #upload = models.FileField(upload_to = 'uploads/equipment/')
