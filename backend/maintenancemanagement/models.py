304de4b1fa6cea848dc6532e3708553b12a89b2d
cassou <cassou@pop-os.localdomain>
2020-04-28 13:55:45 +0200

    ajout model + serializer equipment - non testés

--- backend/maintenancemanagement/models.py
+++ backend/maintenancemanagement/models.py
@@ -14,3 +14,15 @@ class Equipment(models.Model):
         related_query_name="equipment")
     #gestion d'un fichier, voir pour en gérer plusieurs
     #upload = models.FileField(upload_to = 'uploads/equipment/')
+
+class EquipmentType(models.Model):
+    """
+        Define an equipment type
+    """
+    name = models.CharField(max_length=100)
+    field = models.ManyToManyField(Field,
+        verbose_name='Equipment Type Field',
+        blank=True,
+        help_text='Specific fields for this equipment type',
+        related_name="equipmentType_set",
+        related_query_name="equipmentType")
--- backend/maintenancemanagement/serializers.py
+++ backend/maintenancemanagement/serializers.py
@@ -1,3 +1,4 @@
+from .models import Equipment, EquipmentType
 """
 Serializers enable the link between front-end and back-end
 """
@@ -7,3 +8,8 @@ class EquipmentSerializer(serializers.ModelSerializer):
         model = Equipment
         fields = [id, name, equipment_type]
         #ajouter les fichiers par la suite
+
+class EquipmentTypeSerializer(serializer.ModelSerializer):
+    class Meta:
+        model = EquipmentType
+        fields = [id, name, field]
