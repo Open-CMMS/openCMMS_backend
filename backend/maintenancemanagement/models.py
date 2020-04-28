0a4bafcef1094816a25fe0dce9edb5d9a8d61d08
cassou <cassou@pop-os.localdomain>
2020-04-28 14:32:22 +0200

    ajout de les view pour equipment et modification des serializers et models - non testés - #4gr9ap

--- backend/maintenancemanagement/models.py
+++ backend/maintenancemanagement/models.py
@@ -1,5 +1,32 @@
 from django.db import models
 from django.db.models import FileField
+from usersmanagement.models import Team
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+class FieldGroup(models.Model):
+    name = models.CharField(max_length=50)
+    is_equipment = models.BooleanField(default=False)
+
+
+class Field(models.Model):
+    name = models.CharField(max_length=50)
+    field_group = models.ForeignKey(FieldGroup,
+                                    on_delete = models.CASCADE,
+                                    related_name="field_set",
+                                    related_query_name="field")
+
+class EquipmentType(models.Model):
+    """
+        Define an equipment type
+    """
+    name = models.CharField(max_length=100)
+    fields = models.ManyToManyField(Field,
+        verbose_name='Equipment Type Field',
+        blank=True,
+        help_text='Specific fields for this equipment type',
+        related_name="equipmentType_set",
+        related_query_name="equipmentType")
 
 class Equipment(models.Model):
     """
@@ -8,21 +35,9 @@ class Equipment(models.Model):
     name = models.CharField(max_length=100)
     equipment_type = models.ForeignKey(EquipmentType,
         verbose_name = "Equipment Type",
-        on_delete = models.DELETE_CASCADE,
+        on_delete = models.CASCADE,
         null = False,
         related_name = "equipment_set",
         related_query_name="equipment")
     #gestion d'un fichier, voir pour en gérer plusieurs
     #upload = models.FileField(upload_to = 'uploads/equipment/')
-
-class EquipmentType(models.Model):
-    """
-        Define an equipment type
-    """
-    name = models.CharField(max_length=100)
-    field = models.ManyToManyField(Field,
-        verbose_name='Equipment Type Field',
-        blank=True,
-        help_text='Specific fields for this equipment type',
-        related_name="equipmentType_set",
-        related_query_name="equipmentType")
--- backend/maintenancemanagement/serializers.py
+++ backend/maintenancemanagement/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from .models import Equipment, EquipmentType
 """
 Serializers enable the link between front-end and back-end
@@ -6,10 +7,10 @@ Serializers enable the link between front-end and back-end
 class EquipmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Equipment
-        fields = [id, name, equipment_type]
+        fields = ['id', 'name', 'id_equipment_type']
         #ajouter les fichiers par la suite
 
-class EquipmentTypeSerializer(serializer.ModelSerializer):
+class EquipmentTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = EquipmentType
-        fields = [id, name, field]
+        fields = ['id', 'name', 'fields']
