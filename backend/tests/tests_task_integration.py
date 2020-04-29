b3a5329c39e9ff7a2092566a5850c3dd322abce0
cassou <cassou@pop-os.localdomain>
2020-04-29 16:59:14 +0200

    ajout tests task avec end date #4gr9ne

--- backend/tests/tests_task_integration.py
+++ backend/tests/tests_task_integration.py
@@ -174,3 +174,131 @@ class TaskTests(TestCase):
         client.force_authenticate(user=user)
         response = client.delete('/api/maintenancemanagement/tasks/'+str(pk)+'/')
         self.assertEqual(response.status_code,401)
+
+    def test_can_acces_task_list_with_perm_with_end_date(self):
+        """
+            Test if a user with perm receive the data
+        """
+        user = self.set_up_perm()
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        client = APIClient()
+        client.force_authenticate(user=user)
+        response = client.get('/api/maintenancemanagement/tasks/', format='json')
+        self.assertEqual(response.data,serializer.data)
+
+    def test_can_acces_task_list_without_perm_with_end_date(self):
+        """
+            Test if a user without perm doesn't receive the data
+        """
+        user = self.set_up_without_perm()
+        client = APIClient()
+        client.force_authenticate(user=user)
+        response = client.get('/api/maintenancemanagement/tasks/', format='json')
+        self.assertEqual(response.status_code,401)
+
+    def test_add_task_with_perm_with_end_date(self):
+        """
+            Test if a user with perm can add a task
+        """
+        user = self.set_up_perm()
+        client = APIClient()
+        client.force_authenticate(user=user)
+        response = client.post('/api/maintenancemanagement/tasks/', {'name': 'verifier pneus', 'description' : 'faut verfier les pneus de la voiture ta vu', 'end_date': '2020-04-29'}, format='json')
+        self.assertEqual(response.status_code,201)
+        self.assertEqual(response.data['name'], 'verifier pneus')
+
+    def test_add_task_without_perm_with_end_date_with_end_date(self):
+        """
+            Test if a user without perm can't add a task
+        """
+        user = self.set_up_without_perm()
+        client = APIClient()
+        client.force_authenticate(user=user)
+        response = client.post('/api/maintenancemanagement/tasks/', {'name': 'verifier pneus', 'description' : 'faut verfier les pneus de la voiture ta vu', 'end_date': '2020-04-29'}, format='json')
+        self.assertEqual(response.status_code,401)
+
+    def test_view_task_request_with_perm_with_end_date(self):
+        """
+            Test if a user with perm can see a task detail
+        """
+        user = self.set_up_perm()
+        client = APIClient()
+        client.force_authenticate(user=user)
+        response1 = client.post('/api/maintenancemanagement/tasks/', {'name': 'verifier pneus', 'description' : 'faut verfier les pneus de la voiture ta vu','end_date': '2020-04-29'}, format='json')
+        pk = response1.data['id']
+        response = client.get('/api/maintenancemanagement/tasks/'+str(pk)+'/')
+        self.assertEqual(response.data['name'],'verifier pneus')
+
+    def test_view_task_request_without_perm_with_end_date(self):
+        """
+            Test if a user without perm can't see a task detail
+        """
+        user = self.set_up_perm()
+        client = APIClient()
+        client.force_authenticate(user=user)
+        response1 = client.post('/api/maintenancemanagement/tasks/', {'name': 'verifier pneus', 'description' : 'faut verfier les pneus de la voiture ta vu','end_date': '2020-04-29'}, format='json')
+        pk = response1.data['id']
+        user.user_permissions.clear()
+        user = UserProfile.objects.get(id=user.pk)
+        client.force_authenticate(user=user)
+        response = client.get('/api/maintenancemanagement/tasks/'+str(pk)+'/')
+        self.assertEqual(response.status_code,401)
+
+    def test_change_task_with_perm_with_end_date(self):
+        """
+            Test if a user with perm can change a task
+        """
+        user = self.set_up_perm()
+        client = APIClient()
+        client.force_authenticate(user=user)
+        response1 = client.post('/api/maintenancemanagement/tasks/', {'name': 'verifier pneus', 'description' : 'faut verfier les pneus de la voiture ta vu','end_date': '2020-04-29'}, format='json')
+        pk = response1.data['id']
+        response = client.put('/api/maintenancemanagement/tasks/'+str(pk)+'/', {'name':'verifier roues'}, format='json')
+        self.assertEqual(response.data['name'],'verifier roues')
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_change_task_without_perm_with_end_date(self):
+        """
+            Test if a user without perm can change a task
+        """
+        user = self.set_up_perm()
+        client = APIClient()
+        client.force_authenticate(user=user)
+        response1 = client.post('/api/maintenancemanagement/tasks/', {'name': 'verifier pneus', 'description' : 'faut verfier les pneus de la voiture ta vu','end_date': '2020-04-29'}, format='json')
+        pk = response1.data['id']
+        user.user_permissions.clear()
+        user = UserProfile.objects.get(id=user.pk)
+        client.force_authenticate(user=user)
+        response = client.put('/api/maintenancemanagement/tasks/'+str(pk)+'/', {'name':'verifier roues'}, format='json')
+        self.assertEqual(response.status_code, 401)
+
+
+    def test_delete_task_with_perm_with_end_date(self):
+        """
+            Test if a user with perm can delete a task
+        """
+        user = self.set_up_perm()
+        client = APIClient()
+        client.force_authenticate(user=user)
+        response1 = client.post('/api/maintenancemanagement/tasks/', {'name': 'verifier pneus', 'description' : 'faut verfier les pneus de la voiture ta vu','end_date': '2020-04-29'}, format='json')
+        pk = response1.data['id']
+        response = client.delete('/api/maintenancemanagement/tasks/'+str(pk)+'/')
+        self.assertEqual(response.status_code, 204)
+
+
+    def test_delete_task_without_perm_with_end_date(self):
+        """
+            Test if a user without perm can delete a task
+        """
+        user = self.set_up_perm()
+        client = APIClient()
+        client.force_authenticate(user=user)
+        response1 = client.post('/api/maintenancemanagement/tasks/', {'name': 'verifier pneus', 'description' : 'faut verfier les pneus de la voiture ta vu','end_date': '2020-04-29'}, format='json')
+        pk = response1.data['id']
+        user.user_permissions.clear()
+        user = UserProfile.objects.get(id=user.pk)
+        client.force_authenticate(user=user)
+        response = client.delete('/api/maintenancemanagement/tasks/'+str(pk)+'/')
+        self.assertEqual(response.status_code,401)
