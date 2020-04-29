0f8cf9d8e8d227e07bf78f809d5074d7109569c1
cassou <cassou@pop-os.localdomain>
2020-04-29 11:07:18 +0200

    modification model et serializer pour ajouter des fichiers - non testé - #4gr9ap

--- backend/maintenancemanagement/serializers.py
+++ backend/maintenancemanagement/serializers.py
@@ -4,11 +4,6 @@ from .models import Equipment, EquipmentType, Files
 Serializers enable the link between front-end and back-end
 """
 
-class FilesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Files
-        fields = ['id','name','file','is_notice']
-
 class EquipmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Equipment
@@ -18,3 +13,8 @@ class EquipmentTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = EquipmentType
         fields = ['id', 'name', 'fields']
+
+class FilesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Files
+        fields = ['id','name','file','is_noti']
