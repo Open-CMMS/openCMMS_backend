a80c19cc4dbdf78c2c29bd7480d865b42cc0678d
iamodog <joran.mr@icloud.com>
2020-05-06 15:26:38 +0200

    add access to file via /media/.. in DEBUG mode

--- backend/openCMMS/urls.py
+++ backend/openCMMS/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
@@ -24,3 +27,6 @@ urlpatterns = [
     path('api/maintenancemanagement/', include('maintenancemanagement.urls')),
     path('api/docs/', schema_view),
 ]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
