d8f777bfc564f802509a9ae7b81e396339c7bfc0
iamodog <joran.mr@icloud.com>
2020-05-07 16:02:17 +0200

    ajout de content_type et object_id automatique dans le serializer

--- backend/maintenancemanagement/admin.py
+++ backend/maintenancemanagement/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from maintenancemanagement.models import FieldObject
+from maintenancemanagement.models import FieldObject, Field, FieldValue, FieldGroup, Task, Equipment, EquipmentType
 
 # Register your models here.
 
 admin.site.register(FieldObject)
+admin.site.register(Field)
+admin.site.register(FieldValue)
+admin.site.register(FieldGroup)
+admin.site.register(Task)
+admin.site.register(Equipment)
+admin.site.register(EquipmentType)
--- backend/maintenancemanagement/serializers.py
+++ backend/maintenancemanagement/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Equipment, EquipmentType, File, Task, TaskType, FieldValue, Field, FieldObject
-
+from django.contrib.contenttypes.models import ContentType
 
 """
 Serializers enable the link between front-end and back-end
@@ -59,16 +59,19 @@ class DescribedObjectRelatedField(serializers.RelatedField):
             return "TaskType: " + str(value.id)
         raise Exception('Unexpected type of tagged object')
 
+    def to_internal_value(self, data):
+        return data
+
 class FieldObjectSerializer(serializers.ModelSerializer):
-    described_object = DescribedObjectRelatedField(queryset = FieldObject.objects.all())
+    described_object = DescribedObjectRelatedField(queryset= FieldObject.objects.all())
     class Meta:
         model = FieldObject
         fields = ['described_object','field','field_value','value','description']
 
     def create(self, validated_data):
         return FieldObject.objects.create(
-            content_type = ContentType.objects.get(name=self.validated_data['described_object'].partition(":")[0]),
-            object_id = int(self.validated_data['described_object'].partition(":")[1]),
+            content_type = ContentType.objects.get(model=self.validated_data['described_object'].partition(":")[0].lower()),
+            object_id = self.validated_data['described_object'].partition(":")[2],
             field = self.validated_data['field'],
             field_value = self.validated_data['field_value'],
             value = self.validated_data['value'],
