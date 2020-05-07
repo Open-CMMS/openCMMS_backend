f6d6a2d6a7e14685eb7e1dcd1c94351e708b2975
cassou <cassou@pop-os.localdomain>
2020-05-07 10:35:47 +0200

    ajout pour un utilisateur assigné à une tâche de pouvoir la voir + les tests associés - fonctionnels #4gr9d7

--- backend/maintenancemanagement/views/views_task.py
+++ backend/maintenancemanagement/views/views_task.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from maintenancemanagement.serializers import TaskSerializer
 from maintenancemanagement.models import Task, Field, FieldGroup, FieldValue
 from usersmanagement.models import Team, UserProfile
+from usersmanagement.views.views_team import belongs_to_team
 from django.contrib.auth import authenticate, login, logout
 
 @api_view(['GET', 'POST'])
@@ -39,7 +40,7 @@ def task_detail(request, pk):
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        if request.user.has_perm("maintenancemanagement.view_task"):
+        if request.user.has_perm("maintenancemanagement.view_task") or participate_to_task(request.user,task):
             serializer = TaskSerializer(task)
             return Response(serializer.data)
         return Response(status=status.HTTP_401_UNAUTHORIZED)
@@ -133,11 +134,21 @@ def user_task_list(request, pk):
     else :
         return Response(status=status.HTTP_401_UNAUTHORIZED)
 
+def participate_to_task(user,task):
+    """
+        \n# Check if a user is assigned to the task
+
+    """
+    for team in task.teams.values_list("id", flat=True).iterator():
+        belong = belongs_to_team(user,Team.objects.get(id=team))
+        if belong:
+            return True
+    return False
 
 
 def init_database():
     fieldGr = FieldGroup.objects.create(name="Maintenance", is_equipment=False)
-    
+
     fieldCriDec = Field.objects.create(name="Trigger Conditions", field_group=fieldGr)
     fieldCriFin = Field.objects.create(name="End Conditions", field_group=fieldGr)
 
