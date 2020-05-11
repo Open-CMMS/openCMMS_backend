9f6626fc8d5ea18a67654e88497d67a9263cfad7
mauvray <matteo.auvray@insa-rouen.fr>
2020-05-11 18:20:29 +0200

    on finit les tests

--- backend/tests/tests_fieldobject_integration.py
+++ backend/tests/tests_fieldobject_integration.py
@@ -21,22 +21,22 @@ class FieldObjectTests(TestCase):
         field_maintenance = FieldGroup.objects.create(name="Maintenance")
         des_conditions = Field.objects.create(name="Des Conditions", field_group = field_maintenance)
         condition_date = FieldValue.objects.create(value = "Date", field = des_conditions)
-        condition_duree =FieldValue.objects.create(value = "Durée", field = des_conditions)
+        condition_duree = FieldValue.objects.create(value = "Durée", field = des_conditions)
         une_tache = Task.objects.create(name="uneTache")
         typeE = EquipmentType.objects.create(name="Type d'equipement")
         un_equipment = Equipment.objects.create(name="unEquipement", equipment_type=typeE)
-        FieldObject.objects.create(content_type = ContentType.objects.get(model='task'),
-                                   object_id = une_tache.id,
-                                   field = des_conditions,
-                                   field_value= condition_date,
-                                   value = "02/05/2020",
-                                   description = "Date")
-        FieldObject.objects.create(content_type = ContentType.objects.get(model='equipment'),
-                                   object_id = un_equipment.id,
-                                   field = des_conditions,
-                                   field_value= condition_duree,
-                                   value = "02/05/2020",
-                                   description = "Date")
+        FieldObject.objects.create(content_type=ContentType.objects.get(model='task'),
+                                   object_id=une_tache.id,
+                                   field=des_conditions,
+                                   field_value=condition_date,
+                                   value="02/05/2020",
+                                   description="Date")
+        FieldObject.objects.create(content_type=ContentType.objects.get(model='equipment'),
+                                   object_id=un_equipment.id,
+                                   field=des_conditions,
+                                   field_value=condition_duree,
+                                   value="02/05/2020",
+                                   description="Date")
 
     def add_view_perm(self,user):
         """
@@ -51,6 +51,20 @@ class FieldObjectTests(TestCase):
         """
         perm_add = Permission.objects.get(codename="add_fieldobject")
         user.user_permissions.set([perm_add])
+    
+    def add_update_perm(self,user):
+        """
+            Add update permission to user
+        """
+        perm_update = Permission.objects.get(codename="change_fieldobject")
+        user.user_permissions.set([perm_update])
+    
+    def add_delete_perm(self, user):
+        """
+            Add update permission to user
+        """
+        perm_delete = Permission.objects.get(codename="delete_fieldobject")
+        user.user_permissions.set([perm_delete])
 
     def test_fieldObject_list_get_authorized(self):
         """
@@ -113,4 +127,67 @@ class FieldObjectTests(TestCase):
                 "description": "Date de test"
         }
         response = c.post("/api/maintenancemanagement/fieldobjects/", data, format='json')
+        self.assertEqual(response.status_code,401)
+    
+    def test_fieldObject_details_get_authorized(self):
+        user = UserProfile.objects.create(username="user", password="p4ssword")
+        self.add_view_perm(user)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        fo = FieldObject.objects.get(content_type=ContentType.objects.get(model='equipment'))
+        response = c.get(f"/api/maintenancemanagement/fieldobjects/{fo.pk}/")
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.data, FieldObjectSerializer(fo).data)
+        
+    def test_fieldObject_details_get_unauthorized(self):
+        user = UserProfile.objects.create(username="user", password="p4ssword")
+        c = APIClient()
+        c.force_authenticate(user=user)
+        fo = FieldObject.objects.get(content_type=ContentType.objects.get(model='equipment'))
+        response = c.get(f"/api/maintenancemanagement/fieldobjects/{fo.pk}/")
+        self.assertEqual(response.status_code,401)
+
+    def test_fieldObject_details_put_authorized(self):
+        user = UserProfile.objects.create(username="user", password="p4ssword")
+        self.add_update_perm(user)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        fo = FieldObject.objects.get(content_type=ContentType.objects.get(model='equipment'))
+        data = {
+                "described_object": "Task: 1",
+                "field": 1,
+        }
+        response = c.put(f"/api/maintenancemanagement/fieldobjects/{fo.pk}/", data, format="json")
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.data["described_object"], data["described_object"])
+        self.assertEqual(response.data["field"], data["field"])
+    
+    def test_fieldObject_details_put_unauthorized(self):
+        user = UserProfile.objects.create(username="user", password="p4ssword")
+        c = APIClient()
+        c.force_authenticate(user=user)
+        fo = FieldObject.objects.get(content_type=ContentType.objects.get(model='equipment'))
+        data = {
+                "described_object": "Task: 1",
+                "field": 1,
+        }
+        response = c.put(f"/api/maintenancemanagement/fieldobjects/{fo.pk}/", data, format="json")
+        self.assertEqual(response.status_code,401)
+
+    def test_fieldObject_details_delete_authorized(self):
+        user = UserProfile.objects.create(username="user", password="p4ssword")
+        self.add_delete_perm(user)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        fo = FieldObject.objects.get(content_type=ContentType.objects.get(model='equipment'))
+        response = c.delete(f"/api/maintenancemanagement/fieldobjects/{fo.pk}/")
+        self.assertEqual(response.status_code, 204)
+
+    
+    def test_fieldObject_details_delete_unauthorized(self):
+        user = UserProfile.objects.create(username="user", password="p4ssword")
+        c = APIClient()
+        c.force_authenticate(user=user)
+        fo = FieldObject.objects.get(content_type=ContentType.objects.get(model='equipment'))
+        response = c.delete(f"/api/maintenancemanagement/fieldobjects/{fo.pk}/")
         self.assertEqual(response.status_code,401)
