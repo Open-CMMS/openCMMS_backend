8cf66f2e176c8dbbc2b893c22fa6e841675d2d0a
mauvray <matteo.auvray@insa-rouen.fr>
2020-09-23 14:56:24 +0200

    Nettoyage du fichier pour TeamList et TeamDetail

--- usersmanagement/views/views_team.py
+++ usersmanagement/views/views_team.py
@@ -1,3 +1,4 @@
+"""This module expose the Team model."""
 from usersmanagement.models import Team, UserProfile
 from usersmanagement.serializers import TeamSerializer
 
@@ -8,35 +9,62 @@ from rest_framework.views import APIView
 
 
 class TeamList(APIView):
-    """
-        \n# List all the teams or create a new one
+    """# List all the teams or create a new one.
 
+    Parameter :
+    request (HttpRequest) : the request coming from the front-end
+
+    Return :
+    response (Response) : the response.
+
+    GET request : list all teams and return the data
+    POST request :
+    - create a new team, send HTTP 201.  If the request is not valid, send\
+        HTTP 400.
+    - If the user doesn't have the permissions, it will send HTTP 401.
+    - The request must contain name (the name of the team, string) and\
+        team_type (the id of the team_type, int), can contain user_set\
+                (the users' id list, [])
+    """
+
+    def get(self, request, format='None'):
+        """# Implement the GET method.
+
+        ```
         Parameter :
         request (HttpRequest) : the request coming from the front-end
 
         Return :
         response (Response) : the response.
 
-        GET request : list all teams and return the data
-        POST request :
-        - create a new team, send HTTP 201.  If the request is not valid, send\
-            HTTP 400.
-        - If the user doesn't have the permissions, it will send HTTP 401.
-        - The request must contain name (the name of the team, string) and\
-            team_type (the id of the team_type, int), can contain user_set\
-                 (the users' id list, [])
-    """
-
-    def get(self, request, format='None'):
-        """Define what happen if a request is made using GET method."""
+        GET request : list all teams and return the data.
+        ```
+        """
         if request.user.has_perm("usersmanagement.view_team"):
             teams = Team.objects.all()
             serializer = TeamSerializer(teams, many=True)
             return Response(serializer.data)
-        return Response(status=status.HTTP_401_UNAUTHORIZED
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-    def post(self, requets, format='None'):
-        """Define what happen if a request is made using POST method."""
+    def post(self, request, format='None'):
+        """# Implement the POST method.
+
+        ```
+        Parameter :
+        request (HttpRequest) : the request coming from the front-end
+
+        Return :
+        response (Response) : the response.
+
+        POST request :
+        - create a new team, send HTTP 201.  If the request is not valid send \
+HTTP 400.
+        - If the user doesn't have the permissions, it will send HTTP 401.
+        - The request must contain name (the name of the team, string) and\
+team_type (the id of the team_type, int), can contain user_set (the users' id \
+list, []).
+        ```
+        """
         if request.user.has_perm("usersmanagement.add_team"):
             serializer = TeamSerializer(data=request.data)
             if serializer.is_valid():
@@ -48,12 +76,34 @@ class TeamList(APIView):
         return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 
+class TeamDetail(APIView):
+    """# Retrieve, update or delete a team.
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def team_detail(request, pk):
+    Parameters :
+    request (HttpRequest) : the request coming from the front-end
+    pk (int) : the id of the team
+
+    Return :
+    response (Response) : the response.
+
+    GET request : return the team's data.
+    PUT request : change the team with the data on the request or if the data \
+        isn't well formed, send HTTP 400.
+    DELETE request: delete the team and send HTTP 204.
+
+    If the user doesn't have the permissions, it will send HTTP 401.
+    If the id doesn't exist, it will send HTTP 404.
+
+    The PUT request can contain one or more of the following fields :
+        - name (string) : the name of the team
+        - team_type (int) : the id of the team_type
+        - user_set ([]): the users' id
     """
-        \n# Retrieve, update or delete a team
 
+    def get(self, request, pk, format='None'):
+        """# Implements the GET method.
+
+        ```
         Parameters :
         request (HttpRequest) : the request coming from the front-end
         pk (int) : the id of the team
@@ -62,29 +112,37 @@ def team_detail(request, pk):
         response (Response) : the response.
 
         GET request : return the team's data.
-        PUT request : change the team with the data on the request or if the data isn't well formed, send HTTP 400.
-        DELETE request: delete the team and send HTTP 204.
-
-        If the user doesn't have the permissions, it will send HTTP 401.
-        If the id doesn't exist, it will send HTTP 404.
-
-        The PUT request can contain one or more of the following fields :
-            - name (string) : the name of the team
-            - team_type (int) : the id of the team_type
-            - user_set ([]): the users' id
-    """
-    try:
-        team = Team.objects.get(pk=pk)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
+        ```
+        """
+        try:
+            team = Team.objects.get(pk=pk)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
         if request.user.has_perm("usersmanagement.view_team"):
             serializer = TeamSerializer(team)
             return Response(serializer.data)
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format='None'):
+        """# Implements the PUT method.
+
+        ```
+        Parameters :
+        request (HttpRequest) : the request coming from the front-end
+        pk (int) : the id of the team
+
+        Return :
+        response (Response) : the response.
+
+        PUT request : Change the team with the data on the request or if \
+the data isn't well formed, send HTTP 400.
+        ```
+        """
+        try:
+            team = Team.objects.get(pk=pk)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
         if request.user.has_perm("usersmanagement.change_team"):
             serializer = TeamSerializer(team, data=request.data, partial=True)
             if serializer.is_valid():
@@ -92,14 +150,34 @@ def team_detail(request, pk):
                 team = Team.objects.get(pk=serializer.data['id'])
                 team.team_type._apply_()
                 return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format='None'):
+        """# Implements the DELETE method.
+
+        ```
+        Parameters :
+        request (HttpRequest) : the request coming from the front-end
+        pk (int) : the id of the team
+
+        Return :
+        response (Response) : the response.
+
+        DELETE request : delete the team and send HTTP 204.
+        ```
+        """
+        try:
+            team = Team.objects.get(pk=pk)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
         if request.user.has_perm("usersmanagement.delete_team"):
             team.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 
 @api_view(['POST', 'PUT'])
@@ -113,8 +191,12 @@ def add_user_to_team(request):
         Return :
         response (Response) : the response.
 
-        POST request : add a user to a team and send HTTP 201, must contain id_user (the id of the user to add, int) and id_team (the id of the team where the user will be add, int)
-        PUT request : remove a user from a team and send HTTP 201, must contain id_user (the id of the user to remove, int) and id_team (the id of the team where the user will be remove, int)
+        POST request : add a user to a team and send HTTP 201, must contain\
+             id_user (the id of the user to add, int) and id_team (the id of\
+                 the team where the user will be add, int)
+        PUT request : remove a user from a team and send HTTP 201, must\
+            contain id_user (the id of the user to remove, int) and\
+                id_team (the id of the team where the user will be remove, int)
 
         If the user doesn't have the permissions, it will send HTTP 401.
     """
