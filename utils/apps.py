8fc211b2da20ff2b7c4e56a5af5e5f1531c56398
mauvray <matteo.auvray@insa-rouen.fr>
2020-10-07 17:29:04 +0200

    Ajout du start dans apps py
    C'est pour que django lance le scheduler

--- utils/apps.py
+++ utils/apps.py
@@ -1,5 +1,11 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+
 from django.apps import AppConfig
 
 
 class UtilsConfig(AppConfig):
     name = 'utils'
+
+    def ready(self):
+        from utils.notifications import start
+        start()
