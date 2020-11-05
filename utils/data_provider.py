e599df2625d352bda185744c0bd6115b310139ac
huguy <hugo.simoes_maricato@insa-rouen.fr>
2020-11-05 16:23:14 +0100

    Test access db

--- utils/data_provider.py
+++ utils/data_provider.py
@@ -26,6 +26,7 @@ class DataProviderException(Exception):
 
 def start():
     """Initialise all data provider jobs when django starts."""
+    print(DataProvider.objects.all())
     dataproviders = DataProvider.objects.filter(is_activated=True)
     for dataprovider in dataproviders:
         try:
