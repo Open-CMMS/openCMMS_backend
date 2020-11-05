6f00d6a948cc9d58c9741b4ab8303c169c0ea076
huguy <hugo.simoes_maricato@insa-rouen.fr>
2020-11-05 16:56:11 +0100

    Un autre test

--- utils/apps.py
+++ utils/apps.py
@@ -6,7 +6,8 @@ class UtilsConfig(AppConfig):
 
     def ready(self):
         print("Voici les arguments ! :", sys.argv)
-        if 'pytest' not in sys.argv:
+        print("Pytest dans le premier argv : ", 'pytest' in sys.argv[0])
+        if 'pytest' not in sys.argv[0]:
             from utils import notifications
             notifications.start()
             from utils import data_provider
