cab266eabadd82313bdf42870c62a599698c5a8c
huguy <hugo.simoes_maricato@insa-rouen.fr>
2020-11-05 16:51:00 +0100

    Tentative de correction

--- utils/apps.py
+++ utils/apps.py
@@ -1,11 +1,12 @@
 from django.apps import AppConfig
-
+import sys
 
 class UtilsConfig(AppConfig):
     name = 'utils'
 
     def ready(self):
-        from utils import notifications
-        notifications.start()
-        # from utils import data_provider
-        # data_provider.start()
+        if 'pytest' not in sys.argv:
+            from utils import notifications
+            notifications.start()
+            from utils import data_provider
+            data_provider.start()
