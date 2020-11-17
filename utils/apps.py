3b5ae0d971c5d479607fe0280b051f49cf07ed1e
mauvray <matteo.auvray@insa-rouen.fr>
2020-11-17 11:00:13 +0100

    #CU-a2x172 - Corrections flake8 pour tester le logger ensuite

--- utils/apps.py
+++ utils/apps.py
@@ -1,16 +1,18 @@
-import sys
-
+"""This script describes our utils app."""
 from django.apps import AppConfig
 
 
 class UtilsConfig(AppConfig):
+    """The app class."""
+
     name = 'utils'
 
     def ready(self):
+        """Launch our APSchedulers for our background tasks."""
         try:
             from utils import notifications
             notifications.start()
             from utils import data_provider
             data_provider.start()
-        except:
+        except Exception:
             pass
