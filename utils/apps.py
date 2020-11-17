9a53310e3d3fb7564d7cceead7223c86cfb13c76
mauvray <matteo.auvray@insa-rouen.fr>
2020-11-17 10:54:33 +0100

    Revert "#CU-a2x172 - Ajout de log si un des deux schedulers raise un exception et corrections flake8"
    This reverts commit a5f004a0eb3cb909f5ee5fdc321b28a9b9b2786b.

--- utils/apps.py
+++ utils/apps.py
@@ -1,22 +1,16 @@
-"""This script describes our utils app."""
-from logging import getLogger
+import sys
 
 from django.apps import AppConfig
 
-logger = getLogger(__name__)
-
 
 class UtilsConfig(AppConfig):
-    """The app class."""
-
     name = 'utils'
 
     def ready(self):
-        """Launch our APSchedulers for our background tasks."""
         try:
             from utils import notifications
             notifications.start()
             from utils import data_provider
             data_provider.start()
-        except Exception as e:
-            logger.critical("The crons could not be started.\n{}", e)
+        except:
+            pass
