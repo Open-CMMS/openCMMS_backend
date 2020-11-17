a5f004a0eb3cb909f5ee5fdc321b28a9b9b2786b
mauvray <matteo.auvray@insa-rouen.fr>
2020-11-17 10:50:50 +0100

    #CU-a2x172 - Ajout de log si un des deux schedulers raise un exception et corrections flake8

--- utils/apps.py
+++ utils/apps.py
@@ -1,16 +1,22 @@
-import sys
+"""This script describes our utils app."""
+from logging import getLogger
 
 from django.apps import AppConfig
 
+logger = getLogger(__name__)
+
 
 class UtilsConfig(AppConfig):
+    """The app class."""
+
     name = 'utils'
 
     def ready(self):
+        """Launch our APSchedulers for our background tasks."""
         try:
             from utils import notifications
             notifications.start()
             from utils import data_provider
             data_provider.start()
-        except:
-            pass
+        except Exception as e:
+            logger.critical("The crons could not be started.\n{}", e)
