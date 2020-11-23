cae99d826e7634cfb885c538343120e5cbd1699e
huguy <hugo.simoes_maricato@insa-rouen.fr>
2020-11-23 15:19:14 +0100

    Hotfix - Correction de #52 afin de ne pas récupérer les templates comme étant des tâches

--- maintenancemanagement/views/views_task.py
+++ maintenancemanagement/views/views_task.py
@@ -450,7 +450,8 @@ class UserTaskList(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         if request.user.has_perm(VIEW_TASK) or request.user == user:
-            tasks = Task.objects.filter(teams__pk__in=user.groups.all().values_list("id", flat=True).iterator())
+            tasks = Task.objects.filter(teams__pk__in=user.groups.all().values_list("id", flat=True).iterator(),
+                                        is_template=False)
             serializer = TaskListingSerializer(tasks, many=True)
             return Response(serializer.data)
         return Response(status=status.HTTP_401_UNAUTHORIZED)
