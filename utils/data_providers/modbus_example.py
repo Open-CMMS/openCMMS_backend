3acca15f8a61fe888130959b11ac434e4688ca3c
Adrien Michelet <adrien.michelet@insa-rouen.fr>
2020-11-24 15:19:17 +0100

    #CU-aez485 - préparation d'ajout de paramètres à get_data

--- utils/data_providers/modbus_example.py
+++ utils/data_providers/modbus_example.py
@@ -6,12 +6,13 @@ from umodbus.client import tcp
 from utils.data_provider import GetDataException
 
 
-def get_data(ip_address):
+def get_data(ip_address, port=502, memory_address=0, slave_id=1):
+    """get_data is excpected to return a unique value."""
     try:
         conf.SIGNED_VALUES = False
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((ip_address, 5002))
-        message = tcp.read_holding_registers(slave_id=1, starting_address=0, quantity=1)
+        message = tcp.read_holding_registers(slave_id=slave_id, starting_address=memory_address, quantity=1)
         response = tcp.send_message(message, sock)
         sock.close()
         return response[0]
