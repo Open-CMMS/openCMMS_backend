b375533bcc7ccf1c4fdb7cd9b17e45d8bd752250
Adrien Michelet <adrien.michelet@insa-rouen.fr>
2020-12-08 12:21:15 +0100

    #CU-b51n82 - WIP scheduler

--- utils/trigger_tasks.py
+++ utils/trigger_tasks.py
@@ -1,8 +1,9 @@
 """This file allows to trigger tasks with trigger condition."""
 
+from datetime import date
 import logging
 from maintenancemanagement.models import Field, FieldGroup, FieldObject, Task
-
+from utils.methods import parse_time
 from apscheduler.schedulers.background import BackgroundScheduler
 
 logger = logging.getLogger(__name__)
@@ -18,13 +19,15 @@ def at_least_one_conditon_is_verified(task):
     trigger_conditions = Field.objects.filter(field_group=FieldGroup.objects.get(name="Trigger Conditions"))
     task_conditions = FieldObject.objects.filter(field__in=trigger_conditions, described_object=task)
     for condition in task_conditions:
-        if condition_is_verified(condition) is True:
+        if condition_is_verified(condition, task):
             return True
     return False
 
-def condition_is_verified(condition):
-    pass
-    # A CODER
+def condition_is_verified(condition, task):
+    if condition.field.name == 'Recurrence':
+        delay = condition.value.split('|')[1]
+        return date.today() > task.end_date - parse_time(delay)
+    # SUITE A CODER
 
 def start():
     r"""\n# Set up the cron job to trigger tasks."""
