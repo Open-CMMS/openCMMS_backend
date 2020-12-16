ed16485372b61383c181b9fb6866858ebb9d7d02
mauvray <matteo.auvray@insa-rouen.fr>
2020-12-16 16:04:01 +0100

    #CU-bgz1zv - Correction de la documentation des tests d'authentification

--- tests/tests_authenticate_integration.py
+++ tests/tests_authenticate_integration.py
@@ -18,7 +18,14 @@ class AuthentificationTests(TestCase):
 
     def test_is_connected_with_correct_identifier_but_no_team(self):
         """
-            Test if a user with correct identifier can connect
+        Test if a user with correct identifier can connect.
+
+                Inputs:
+                    user (UserProfile): A UserProfile we create with no team assigned.
+
+                Expected outputs:
+                    We expect a 200 status code in the response.
+                    We expect to find the pair {'error': 'User has no team'} in the error of the response's data.
         """
 
         user = self.set_up()
@@ -28,11 +35,19 @@ class AuthentificationTests(TestCase):
             'username=tom&password=truc',
             content_type='application/x-www-form-urlencoded'
         )
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['error']['error'], 'User has no team')
 
     def test_is_connected_with_correct_identifier(self):
         """
-            Test if a user with correct identifier can connect
+        Test if a user with correct identifier can connect.
+
+                Inputs:
+                    user (UserProfile): a UserProfile we created and that is associated with team.
+                    team (Team): a Team created so that user could be assigned to a Team.
+
+                Excpected outputs:
+                    We expect to find the pair {'success': 'True'} in the data of the response's data.
         """
 
         user = self.set_up()
@@ -46,11 +61,18 @@ class AuthentificationTests(TestCase):
             'username=tom&password=truc',
             content_type='application/x-www-form-urlencoded'
         )
-        self.assertEqual(response.data.get("data").get("success"), 'True')
+        self.assertEqual(response.data['data']['success'], 'True')
 
     def test_is_not_connected_with_incorrect_identifier(self):
         """
-            Test if a user with incorrect identifier can't connect
+        Test if a user with incorrect identifier can't connect.
+
+                Inputs:
+                    user (UserProfile): a UserProfile we created and that we will try to login with the wrong password.
+
+                Expected Outputs:
+                    We expect to find the pair {'success': 'False'} in the error of the response's data.
+                    We expect to find the pair {'error': 'Incorrect Password'} in the error of the response's data.
         """
         user = self.set_up()
         client = APIClient()
@@ -59,17 +81,32 @@ class AuthentificationTests(TestCase):
             'username=tom&password=machin',
             content_type='application/x-www-form-urlencoded'
         )
-        self.assertEqual(response.data.get("error").get("success"), 'False')
-        self.assertEqual(response.data.get("error").get("error"), 'Incorrect password')
+        self.assertEqual(response.data['error']['success'], 'False')
+        self.assertEqual(response.data['error']['error'], 'Incorrect password')
 
     def test_is_not_connected_nbtries_3(self):
         """
-            Test if a user trying to connect three times with wrong password has a number of tries to three.
+        Test if a user trying to connect three times with wrong password has a number of tries to three.
+
+                Inputs:
+                    user (UserProfile): a UserProfile we created and that we will block by sending three times a wrong password.
+
+                Expected Outputs:
+                    We expect to find the pair {'success': 'False'} in the error of the response's data.
+                    We expect to find the pair {'is_blocked': 'True'} in the error of the response's data.
+                    We expect to find the pair {'error': 'Wrong password 3 times in a row. You are blocked.'} in the error of the response's data.
+                    We expect user to have its nb_tries equals to 3.
+                    We expect user to have its is_actiave to be False.
         """
 
         user = self.set_up()
         client = APIClient()
-        response = client.post(
+        client.post(
+            '/api/usersmanagement/login',
+            'username=tom&password=machin',
+            content_type='application/x-www-form-urlencoded'
+        )
+        client.post(
             '/api/usersmanagement/login',
             'username=tom&password=machin',
             content_type='application/x-www-form-urlencoded'
@@ -79,58 +116,36 @@ class AuthentificationTests(TestCase):
             'username=tom&password=machin',
             content_type='application/x-www-form-urlencoded'
         )
-        response = client.post(
-            '/api/usersmanagement/login',
-            'username=tom&password=machin',
-            content_type='application/x-www-form-urlencoded'
-        )
-        self.assertEqual(response.data.get("error").get("success"), 'False')
-        self.assertEqual(response.data.get("error").get("is_blocked"), 'True')
-        self.assertEqual(response.data.get("error").get("error"), "Wrong password 3 times in a row. You are blocked.")
+        self.assertEqual(response.data['error']['success'], 'False')
+        self.assertEqual(response.data['error']['is_blocked'], 'True')
+        self.assertEqual(response.data['error']['error'], 'Wrong password 3 times in a row. You are blocked.')
 
         user = UserProfile.objects.get(pk=user.pk)
         self.assertEqual(user.nb_tries, 3)
-
-    def test_is_blocked_nbtries_3(self):
-        """
-            Test if a user trying to connect three times with wrong password is blocked.
-        """
-        user = self.set_up()
-        client = APIClient()
-        response = client.post(
-            '/api/usersmanagement/login',
-            'username=tom&password=machin',
-            content_type='application/x-www-form-urlencoded'
-        )
-        response = client.post(
-            '/api/usersmanagement/login',
-            'username=tom&password=machin',
-            content_type='application/x-www-form-urlencoded'
-        )
-        response = client.post(
-            '/api/usersmanagement/login',
-            'username=tom&password=machin',
-            content_type='application/x-www-form-urlencoded'
-        )
-        self.assertEqual(response.data.get("error").get("success"), 'False')
-        self.assertEqual(response.data.get("error").get("is_blocked"), 'True')
-        self.assertEqual(response.data.get("error").get("error"), "Wrong password 3 times in a row. You are blocked.")
-
-        user = UserProfile.objects.get(pk=user.pk)
-        self.assertEqual(user.is_active, False)
+        self.assertIs(user.is_active, False)
 
     def test_is_blocked_cant_connect(self):
         """
-            Test if a blocked user can't connect with correct identifier.
+        Test if a blocked user can't connect with correct identifier.
+
+                Inputs:
+                    user (UserProfile): a UserProfile we created and that we will block by sending three times a wrong password.
+                        We will attempt a fourth connection with the right data.
+
+                Expected Outputs:
+                    We expect to find the pair {'success': 'False'} in the error of the response's data.
+                    We expect to find the pair {'is_blocked': 'True'} in the error of the response's data.
+                    We expect to find the pair {'error': 'You have entered a wrong password too many times. You are blocked.'} in the error of the response's data.
+
         """
         user = self.set_up()
         client = APIClient()
-        response = client.post(
+        client.post(
             '/api/usersmanagement/login',
             'username=tom&password=machin',
             content_type='application/x-www-form-urlencoded'
         )
-        response = client.post(
+        client.post(
             '/api/usersmanagement/login',
             'username=tom&password=machin',
             content_type='application/x-www-form-urlencoded'
@@ -145,7 +160,7 @@ class AuthentificationTests(TestCase):
         self.assertEqual(response.data.get("error").get("is_blocked"), 'True')
         self.assertEqual(response.data.get("error").get("error"), "Wrong password 3 times in a row. You are blocked.")
         user = UserProfile.objects.get(pk=user.pk)
-        self.assertEqual(user.nb_tries, 3)
+        self.assertEqual(user.nb_tries, 3)  # we let those assert to be sure that this step is valid before pursuing.
 
         response = client.post(
             '/api/usersmanagement/login',
@@ -153,16 +168,21 @@ class AuthentificationTests(TestCase):
             content_type='application/x-www-form-urlencoded'
         )
 
-        self.assertEqual(response.data.get("error").get("success"), 'False')
-        self.assertEqual(response.data.get("error").get("is_blocked"), 'True')
+        self.assertEqual(response.data['error']['success'], 'False')
+        self.assertEqual(response.data['error']['is_blocked'], 'True')
         self.assertEqual(
-            response.data.get("error").get("error"),
-            "You have entered a wrong password too many times. You are blocked."
+            response.data['error']['error'], 'You have entered a wrong password too many times. You are blocked.'
         )
 
     def test_sign_out(self):
         """
-            Test if a user can logout
+        Test if a user can logout.
+
+                Inputs:
+                    user (UserProfile): a UserProfile we created and that we will connect before disconnecting.
+
+                Expected Output:
+                    We expect to find True in the response's data.
         """
         self.set_up()
         factory = APIRequestFactory()
@@ -172,4 +192,4 @@ class AuthentificationTests(TestCase):
         request.session.save()
         sign_out = SignOut()
         response = sign_out.get(request)
-        self.assertEqual(response.data, True)
+        self.assertIs(response.data, True)
