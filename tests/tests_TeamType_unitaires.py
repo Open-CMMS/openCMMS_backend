20e540c3e928b4b689c9d3823046c29abfc6e67b
Adrien Michelet <adrien.michelet@insa-rouen.fr>
2020-12-17 15:33:04 +0100

    #CU-bgz1zv - Ajout de la docs pour les tests de team type

--- tests/tests_TeamType_unitaires.py
+++ tests/tests_TeamType_unitaires.py
@@ -7,7 +7,7 @@ class TeamTypeTests(TestCase):
 
     def setUp(self):
         """
-            Set up permissions, team, team type for the tests
+            Set up permissions, team, team type for the tests.
         """
         perm_1 = Permission.objects.get(id=1)
         perm_2 = Permission.objects.get(id=2)
@@ -18,14 +18,27 @@ class TeamTypeTests(TestCase):
 
     def test_US1_U1_create_get_team_type(self):
         """
-            Test the creation of a team type in the model
+            Test the creation of a team type in the model.
+
+            Inputs:
+                MaintenanceManager_type (TeamType): a team type to create.
+
+            Expected Output:
+                We expect to find the created team type.
         """
         MaintenanceManager_type = TeamType.objects.create(name="Maintenance Manager")
         self.assertEqual(MaintenanceManager_type, TeamType.objects.get(name="Maintenance Manager"))
 
     def test_US1_U2_add_perm(self):
         """
-            Test the addition of a permission on a team type
+            Test the addition of a permission on a team type.
+
+            Inputs:
+                MaintenanceManager_type (TeamType): a team type to add a perm.
+                perm (Permission): a perm to add to the team type.
+
+            Expected Output:
+                We expect to find that the team type has the perm.
         """
         perm = Permission.objects.get(id=1)
         MaintenanceManager_type = TeamType.objects.create(name="Maintenance Manager")
@@ -34,7 +47,14 @@ class TeamTypeTests(TestCase):
 
     def test_US1_U3_apply_(self):
         """
-            Test add a set of permissions to a set of teams
+            Test add a set of permissions to a set of teams.
+
+            Inputs:
+                admin_type (TeamType): a team type with perms.
+                admin_team (Team): a team of team type admin_type.
+
+            Expected Output:
+                We expect to find that admin_team has perms of admin_type.
         """
         admin_type = TeamType.objects.get(name="Administrators")
         admin_type._apply_()
@@ -44,7 +64,14 @@ class TeamTypeTests(TestCase):
 
     def test_US1_U4_apply_change_teamtype(self):
         """
-            Test change teamtype
+            Test change teamtype.
+
+            Inputs:
+                admin_type (TeamType): a team type with perms.
+                admin_team (Team): a team of team type admin_type.
+
+            Expected Output:
+                We expect to find that admin_team the added perm of admin_type.
         """
         perm_3 = Permission.objects.get(id=3)
         MaintenanceManager_type = TeamType.objects.create(name="Maintenance Manager")
